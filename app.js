7c253f7fd2fd85c7cba87dc33e3ba28bffc15477
Gustavoproyect <gustavinno@gmail.com>
2024-12-17 12:24:03 +0100

    actualizacion

---
+++ app.js
@@ -0,0 +1,76 @@
+const URL='https://fpaniaguajavascript.github.io/movies-250.json';
+
+function generateCard(pelicula){
+    
+    //1. Crear la tarjeta
+    const nuevaCard = document.createElement("div");//Crea un elemento de tipo div
+    nuevaCard.setAttribute("class","card");
+
+    //2. Crear la imagen
+    const nuevaImg = document.createElement("img");
+    nuevaImg.setAttribute("src", pelicula.Poster);
+    nuevaImg.setAttribute("alt", `Póster de la película ${pelicula.Title}`);
+    nuevaCard.appendChild(nuevaImg);
+
+    //3. Crear el contenido de la tarjeta
+    const nuevoContenido = document.createElement("div");
+    nuevoContenido.setAttribute("class","card-content");
+    nuevaCard.appendChild(nuevoContenido);
+
+    //4. Crear el h3 del título <h3>El Padrino</h3>
+    const nuevoTitulo = document.createElement("h3");
+    nuevoTitulo.textContent = pelicula.Title;
+    nuevoContenido.appendChild(nuevoTitulo);
+
+    //5. Crear el director <p><strong>Director:</strong> Francis Ford Coppola</p>
+    const nuevoParrafoDirector = document.createElement("p");
+    const nuevaNegrita = document.createElement("strong");
+    nuevoParrafoDirector.appendChild(nuevaNegrita);
+    nuevaNegrita.textContent = "Director: ";
+    nuevoContenido.appendChild(nuevoParrafoDirector);
+    const textoDirector = document.createTextNode(pelicula.Director)
+    nuevoParrafoDirector.appendChild(textoDirector);
+
+    //6. Año
+    const nuevoParrafoAnyo = document.createElement('p');
+    const nuevaCelda = document.createElement("strong");
+    nuevoParrafoAnyo.appendChild(nuevaCelda);
+    nuevaCelda.textContent = "Director: ";
+    nuevoContenido.appendChild(nuevoParrafoAnyo);
+    const textoAnyo = document.createTextNode(pelicula.textoAnyo)
+    nuevoParrafoAnyo.appendChild(textoAnyo);
+    //7. Genero
+
+
+    
+    //Último paso: Agregar al contenedor la ficha recién creada
+    document.querySelector("#container").appendChild(nuevaCard);//Agregamos el div al contenedor
+}
+
+function processMovie(data) {
+    const peliculas = data.movies;
+    //FORMAS DE RECORRER ARRAYS Y OBJETOS
+    /*
+    //Recorremos con bucle for tradicional
+    for (let i=0;i<peliculas.length;i++){
+        console.log("Duration:" + peliculas[i].Runtime);
+    }
+    //Recorremos con bucle for-of
+    for (pelicula of peliculas) {
+        console.log("Director:" + pelicula.Director);
+    }
+    //Recorremos con el método forEach
+    peliculas.forEach(pelicula => {
+        console.log("Título:" + pelicula.Title);
+    });
+    //Recorre el contenido de un objeto
+    for (atributo in peliculas[0]){
+        console.log(atributo, peliculas[0][atributo]);
+    }
+    */
+    peliculas.forEach(pelicula => {
+        generateCard(pelicula);
+    });
+}
+
+doGetRequest(URL, processMovie);
